e20e8267186592c013f3f4fc9a3ff54b91d1e660
Arthur Jahn <stutrzbecher@gmail.com>
2023-11-07 16:39:57 -0300

    add selfie validation files

--- app/packs/entrypoints/extra_user_fields_validations.js
+++ app/packs/entrypoints/extra_user_fields_validations.js
@@ -1,50 +1,62 @@
 import * as faceapi from 'face-api.js';
 
 const MIN_CONFIDENCE = 0.5;
-const FACE_MANIFEST = 'src/decidim/extra_user_fields/weights/';
+const FACE_MANIFEST = 'http://localhost:3001/';
 
 const initValidations = async () => {
   const resultField = document.querySelector('.face-validation-field input');
   const canvas = document.querySelector('.face-validation-field canvas');
+  const imageTag = document.querySelector('.face-validation-field img');
   const imageField  = document.querySelector('.face-file-field input');
   const detector = new faceapi.SsdMobilenetv1Options({ MIN_CONFIDENCE });
+
   await faceapi.nets.ssdMobilenetv1.load(FACE_MANIFEST);
+  await faceapi.loadFaceLandmarkModel(FACE_MANIFEST);
   await faceapi.loadFaceRecognitionModel(FACE_MANIFEST);
 
   const imageChanged = async () => {
 
+    const imgFile = imageField.files[0];
+    const img = await faceapi.bufferToImage(imgFile);
+    imageTag.src = img.src;
+
     const fullFaceDescriptions = await faceapi
-      .detectAllFaces(imageField, getFaceDetectorOptions())
+      .detectAllFaces(imageTag, detector)
       .withFaceLandmarks()
-      .withFaceDescriptors()
+      .withFaceDescriptors();
 
     if (!fullFaceDescriptions.length) {
-      return
+      return;
     }
 
     // create FaceMatcher with automatically assigned labels
     // from the detection results for the reference image
-    const faceMatcher = new faceapi.FaceMatcher([fullFaceDescriptions[0]])
+    const faceMatcher = new faceapi.FaceMatcher([fullFaceDescriptions[0]]);
 
-    faceapi.matchDimensions(canvas, imageField)
+    faceapi.matchDimensions(canvas, imageTag);
     // resize detection and landmarks in case displayed image is smaller than
     // original size
-    const resizedResults = faceapi.resizeResults(fullFaceDescriptions, imageField)
+    const resizedResults = faceapi.resizeResults(fullFaceDescriptions, imageTag);
     // draw boxes with the corresponding label as text
     const labels = faceMatcher.labeledDescriptors
-      .map(ld => ld.label)
+      .map(ld => ld.label);
   
     if(resizedResults.length !== 2) {
-      alert('upload an image with two faces')
+      // validar imagens sem a identificação de dois rostos.
     } else {
-      resizedResults.forEach(({ detection, descriptor }) => {
-        const label = faceMatcher.findBestMatch(descriptor).toString()
-        const options = { label }   
-        const drawBox = new faceapi.draw.DrawBox(detection.box, options)
-        drawBox.draw(canvas)
-      })
+      const labels = resizedResults.map(({ detection, descriptor }) => {
+        const result = faceMatcher.findBestMatch(descriptor);
+	const label = result._label;
+	const distance = result._distance;
+        const options = { label };
+        const drawBox = new faceapi.draw.DrawBox(detection.box, options);
+        // drawBox.draw(canvas);
+	return { label, distance };
+      });
     }
   }
+
+  
 
   imageField.onchange = imageChanged;
 }
