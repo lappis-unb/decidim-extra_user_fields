2c10a27c4007c4e87ded89f5206f8fc2889bed2c
Eduardo Nunes <eduardonunes2525@gmail.com>
2024-08-19 11:48:22 -0300

    feat: Update translations and add RNE option

--- lib/decidim/extra_user_fields/engine.rb
+++ lib/decidim/extra_user_fields/engine.rb
@@ -12,7 +12,7 @@ module Decidim
       isolate_namespace Decidim::ExtraUserFields
 
       DEFAULT_GENDER_OPTIONS = [:male, :female, :other].freeze
-      DEFAULT_DOCUMENT_TYPE_OPTIONS = [:passport, :DNI].freeze
+      DEFAULT_DOCUMENT_TYPE_OPTIONS = [:passport, :DNI, :RNE].freeze
 
       routes do
         # Add engine routes here
